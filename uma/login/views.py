b9b96f517412d7df02620ac88d8cd57fb07c104d
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-07-12 16:39:06 +0900

    commit

--- uma/login/views.py
+++ uma/login/views.py
@@ -1,5 +1,5 @@
 from django.http
-from django.http.response import HttpResponse from HttpResponse
+from django.http.response import HttpResponse
 
 def index(request):
     return HttpResponse("로그인 아이디와 패스워드를 확인해주세요.")
