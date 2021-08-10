41e4659e1ad1ab167fc8dda522a1827320b48d58
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-08-10 19:29:03 +0900

    commit

--- uma/config/urls.py
+++ uma/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', include('login.urls')),
+    path('question/create/', views.question_create, name='question_create'),
 ]
